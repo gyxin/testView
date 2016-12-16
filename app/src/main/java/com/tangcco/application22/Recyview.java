512eb1d29c14c661c862346304951c6d822b85da
gdd <gyxin87@163.com>
2016-12-17 00:00:26 +0800

    第二次提交

---
+++ app/src/main/java/com/tangcco/application22/Recyview.java
@@ -0,0 +1,13 @@
+package com.tangcco.application22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recyview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyview);
+    }
+}
